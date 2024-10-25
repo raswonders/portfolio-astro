7e024709b70e5337f295cbff8bca3c31104ec909
Rastislav Hepner <15527036+raswonders@users.noreply.github.com>
2024-10-25 18:53:51 +0200

    Revert "Netlify functions test"

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  server: {port: 3000, host: true},
-  adapter: netlify()
+  server: {port: 3000, host: true}
 });
