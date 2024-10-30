03d239cd47d889a3f49bc95b6dc661eb3222caeb
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-10-30 13:22:01 +0100

    use passThroughImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
@@ -10,5 +10,8 @@ export default defineConfig({
   integrations: [tailwind()],
   server: {port: 3000, host: true},
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
+  image: {
+    service: passthroughImageService()
+  }
 });
